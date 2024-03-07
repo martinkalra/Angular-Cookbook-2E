3b489da4d8d74a4d30907da73e8c6aa0257e29c0
martinkalra <martin.kalra@hotmail.com>
2024-03-07 18:43:22 +0000

    implmented component communication

--- start/apps/chapter01/cc-inputs-outputs/src/app/app.component.ts
+++ start/apps/chapter01/cc-inputs-outputs/src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  notificationsCount = 10;
+
+  updateNotificationsCount(count: number) {
+    this.notificationsCount = count;
+  }
+}
